6d6bec2dceb8f89554e161dac8dab874ba53bd58
Einari Tuukkanen <einari.tuukkanen@gmail.com>
2021-05-08 00:36:56 +0300

    Noccchain, login

--- back/app.py
+++ back/app.py
@@ -1,11 +1,12 @@
-import uvicorn
+import base64
 
 from typing import Optional
-
-import pymongo
 from bson.objectid import ObjectId
 
-from fastapi import FastAPI, HTTPException, Body, Request
+import uvicorn
+import pymongo
+
+from fastapi import FastAPI, HTTPException, Body, Request, File
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -49,20 +50,36 @@ def post_user(user: User):
     return str(u.inserted_id)
 
 
+@app.get("/users/")
+def get_users():
+    users = db.users.find()
+    return [{ '_id': str(u['_id']), 'username': u['username'] } for u in list(users)]
+
+
 @app.get("/users/find")
-def get_user(username: str = ''):
+def find_user(username: str = ''):
     u = db.users.find_one({ 'username': username })
+    # TODO: only return public data
+    return {**u, '_id': str(u['_id'])}
+
+
+@app.post("/users/login")
+def login_user(username: str = Body(...), password: str = Body(...)):
+    u = db.users.find_one({ 'username': username })
+    if u['password'] != password:
+        raise HTTPException(status_code=401, detail="No permission")
     return {**u, '_id': str(u['_id'])}
 
 
 @app.get("/users/{user_id}")
 def get_user(user_id: str):
     u = db.users.find_one({ '_id': ObjectId(user_id) })
+    # TODO: only return public data
     return {**u, '_id': str(u['_id'])}
 
 
 @app.post("/nocccoins/transfer")
-def send_noccoins(from_id: str = Body(...), password: str = Body(...), to_id: str = Body(...), amount: int = Body(...)):
+def transfer_nocccoins(from_id: str = Body(...), password: str = Body(...), to_id: str = Body(...), amount: int = Body(...)):
     from_u = db.users.find_one({ '_id': ObjectId(from_id) })
     if not from_u['password'] == password:
         raise HTTPException(status_code=401, detail="No permission")
@@ -75,10 +92,38 @@ def send_noccoins(from_id: str = Body(...), password: str = Body(...), to_id: st
 
 
 @app.post("/nocccoins/add")
-def send_noccoins(to_id: str = Body(...), amount: int = Body(...)):
-    to_u = db.users.find_one({ '_id': ObjectId(to_id) })
-    _set_nocccoins(to_id, to_u['nocccoins'] + amount)
-    return get_user(to_id)
+def add_nocccoins(user_id: str = Body(...), amount: int = Body(...)):
+    to_u = db.users.find_one({ '_id': ObjectId(user_id) })
+    _set_nocccoins(user_id, to_u['nocccoins'] + amount)
+    return get_user(user_id)
+
+
+@app.get("/nocccoins/{noccchain_id}")
+def get_nocccoin(noccchain_id: int):
+    u = db.noccchain.find_one({ 'noccchain_id': noccchain_id })
+    return {**u, '_id': str(u['_id'])}
+
+
+@app.post("/nocccoins/mine")
+def mine_nocccoins(user_id: str = Body(...), image: bytes = Body(...)):
+    # Kuvan normalisointi jos frontti ei tee sitä
+    # Ollin koodille --> Onko kuvassa QR koodi --> status (ja kuva) takaisin
+    # Kuva (ollin tai original) leilalle --> status, mikä nocco
+    # Jos molemmat ok, tallennetaan kuva ja lisätään noccchainiin
+    # Jos ei niin hv
+    # Note: pyyntöjen jonotus / lock
+    # Userille flavour (set)
+    # Noccchain --> id, user_id
+    flavors = [1] # TODO: replace with Leila's code
+    if len(flavors) == 0:
+        raise HTTPException(status_code=404, detail="Nocco not found")
+    add_nocccoins(user_id)
+    noccchain_id = db.noccchain.count() + 1
+    mined = db.noccchain.insert_one({ 'noccchain_id': noccchain_id, 'user_id': user_id })
+    with open(f"noccchain/{noccchain_id}.png", "wb") as fh:
+        fh.write(base64.decodebytes(image))
+    
+    return get_nocccoin(noccchain_id)
 
 
 def main():
