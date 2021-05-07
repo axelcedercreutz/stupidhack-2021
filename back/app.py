200c7a0a9ae40c07df39ac00ece62fe6f55482c4
Einari Tuukkanen <einari.tuukkanen@gmail.com>
2021-05-07 21:18:33 +0300

    Adds user endpoints

--- back/app.py
+++ back/app.py
@@ -3,13 +3,35 @@ import uvicorn
 from typing import Optional
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+# TODO: replace with db
+users = []
+
+
+class User(BaseModel):
+    user_id: int
+    nickname: str
+    password: str
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/users/")
+def read_root(user: User):
+    users.append(user)
+    return user
+
+
+@app.get("/users/{user_id}")
+def read_root(user_id: int):
+    return next((user for user in users if user.user_id == user_id))
 
 
 def main():
