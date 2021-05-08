0dc13bb65e4e39fe61f0c7255912374a167e5145
Olli Kiikkilä <kiikkila.olli@gmail.com>
2021-05-08 14:42:26 +0300

    MinePage

--- front/src/components/Mine.js
+++ front/src/components/Mine.js
@@ -1,48 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { Card, CardContent, Typography, makeStyles } from '@material-ui/core';
+import React from 'react';
+import { Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 import { Page } from '../styles';
-import noccocoinsService from '../services/noccocoins';
 
 const Mine = () => {
-  const classes = useStyles();
-  const [image, setImage] = useState();
-
-  useEffect(() => getMineableNocco(), [image]);
-
-  const getMineableNocco = async () => {
-    const latestNoccchain = await noccocoinsService.getMineNocco();
-    setImage(latestNoccchain);
-  };
-
   return (
-    !!image && (
-      <Page>
-        <Typography variant="h4" component="h2" align="center" padding="20">
-          Mine NOCCCoins
-        </Typography>
+    <Page>
+      <Typography variant="h4" component="h2" align="center" padding="20">
+        Latest block
+      </Typography>
 
-        <Link to="/mine/photo">Submit new photo</Link>
+      <Link to="/mine/photo">Submit new block</Link>
 
-        <div className={classes.root}>
-          <Card>
-            <CardContent>
-              <img src={image} />
-            </CardContent>
-          </Card>
-        </div>
-      </Page>
-    )
+      <Image src="/nocccoins/mine" />
+    </Page>
   );
 };
 
-const useStyles = makeStyles({
-  root: {
-    display: 'grid',
-    gridTemplateColumns: 'auto auto auto',
-    gridGap: 32,
-  },
-});
+const Image = styled.img`
+  width: 100%;
+  padding-top: 3rem;
+  object-fit: contain;
+`;
 
 export default Mine;
