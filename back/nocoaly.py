c932564489608725eef21cbc3047b410e77a0ebe
eipou <eipou@vt>
2021-05-08 12:02:31 +0300

    Add leilas code to mine

--- back/nocoaly.py
+++ back/nocoaly.py
@@ -2,30 +2,26 @@ import requests
 
 flavours = ["blood orange de", "caribbean", "carnival exotic", "cola", "ice soda", "legenda soda", "limon del sol", "mango del sol", "melon crush", "miami strawberry", "päärynä", "rasberry blast", "sunny soda", "tropical"]
 
-
-
-def nocoaly(imageFilePath):
-    
+def find_noccos(image):
     key = '7c6be91133734a54bb820054054cb22e'
     url = 'https://nocco.cognitiveservices.azure.com/customvision/v3.0/Prediction/364c483e-0d8f-4080-a1c8-cb8fca10f4dd/detect/iterations/Iteration1/image'
     
-    with open(imageFilePath, 'rb') as imageFile:
-        f = imageFile.read()
-        imageData = bytearray(f)
-
-    response = requests.post(url, headers={"Prediction-Key": key, "Content-Type": "application/octet-stream"}, data=imageData)
+    response = requests.post(url, headers={"Prediction-Key": key, "Content-Type": "application/octet-stream"}, data=image)
     
-    foundTagnames = [] 
+    foundNoccos = [] 
     responseData = response.json()
     if 'predictions' in responseData.keys():
         for prediction in responseData["predictions"]:
             if prediction['probability'] >= 0.5:
                 tagname = prediction['tagName']
                 if tagname in flavours:
-                    foundTagnames.append(tagname)
-                
-    return foundTagnames
+                    foundNoccos.append(tagname)
+    return foundNoccos
 
 if __name__ == "__main__":
     #path = input("path: ")
-    print(nocoaly("testi1.jpg"))
+    path = "testi1.jpg"
+    with open(path, 'rb') as imageFile:
+        f = imageFile.read()
+        image = bytearray(f)
+    print(nocoaly(image))
