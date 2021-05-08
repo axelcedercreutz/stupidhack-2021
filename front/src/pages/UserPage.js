4be41453879cd82aee83262cf5e97902610bba5d
Axel Cedercreutz <axelcedercreutz@users.noreply.github.com>
2021-05-08 15:15:12 +0300

    fix for UserPage

--- front/src/pages/UserPage.js
+++ front/src/pages/UserPage.js
@@ -20,41 +20,39 @@ const UserPage = () => {
     setMessages(allMessages);
   };
 
-  return (
-    userInfo && (
-      <Page>
+  return userInfo ? (
+    <Page>
+      <div>
         <div>
-          <div>
-            <Dashboard />
-          </div>
-          <div>
-            <Card raised={true}>
-              <CardContent>
-                <Typography>Visit your friends:</Typography>
-                {friends.map(friend => {
-                  return (
-                    <Link to={`/friends/${friend._id}`}>{friend.username}</Link>
-                  );
-                })}
-              </CardContent>
-            </Card>
-            {messages && (
-              <Card>
-                <Typography>Your latest messages:</Typography>
-                {messages.reverse().map(message => (
-                  <Typography>
-                    From: {message.from_id === userId ? 'You' : message.from_id}{' '}
-                    To: {message.to_id === userId ? 'You' : message.to_id}{' '}
-                    Message: {message.message}
-                  </Typography>
-                ))}
-              </Card>
-            )}
-          </div>
+          <Dashboard />
         </div>
-      </Page>
-    )
-  );
+        <div>
+          <Card raised={true}>
+            <CardContent>
+              <Typography>Visit your friends:</Typography>
+              {friends.map(friend => {
+                return (
+                  <Link to={`/friends/${friend._id}`}>{friend.username}</Link>
+                );
+              })}
+            </CardContent>
+          </Card>
+          {messages && (
+            <Card>
+              <Typography>Your latest messages:</Typography>
+              {messages.reverse().map(message => (
+                <Typography>
+                  From: {message.from_id === userId ? 'You' : message.from_id}{' '}
+                  To: {message.to_id === userId ? 'You' : message.to_id}{' '}
+                  Message: {message.message}
+                </Typography>
+              ))}
+            </Card>
+          )}
+        </div>
+      </div>
+    </Page>
+  ) : null;
 };
 
 export default UserPage;
