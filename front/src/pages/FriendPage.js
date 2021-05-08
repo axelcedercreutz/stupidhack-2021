83aed75e4ce4d5ba9e87175047e97d86b13c1f5e
Axel Cedercreutz <axelcedercreutz@users.noreply.github.com>
2021-05-08 13:42:46 +0300

    transfer ok

--- front/src/pages/FriendPage.js
+++ front/src/pages/FriendPage.js
@@ -1,11 +1,9 @@
 import { useRouteMatch } from 'react-router-dom';
-
-import noccocoinsService from '../services/noccocoins';
-import useStore from '../store';
 import Friend from '../components/Friend';
+import useStore from '../store';
 
 const FriendPage = () => {
-  const { friends, userId, userToken, userInfo, setUserInfo } = useStore();
+  const { friends } = useStore();
 
   const match = useRouteMatch('/friends/:id');
 
@@ -13,28 +11,7 @@ const FriendPage = () => {
     ? friends.find(friend => friend._id === match.params.id)
     : undefined;
 
-  const handleTransfer = async () => {
-    const response = await noccocoinsService.transferCoins(
-      userToken,
-      userId,
-      '6095b3f1e0eccae5569a3c94',
-      2,
-    );
-    console.log(response);
-    const newUserInfo = {
-      ...userInfo,
-      nocccoins: response.nocccoins,
-    };
-    setUserInfo(newUserInfo);
-  };
-
-  return (
-    <Friend
-      friend={friend}
-      userId={userId}
-      handleTransfer={() => handleTransfer()}
-    />
-  );
+  return <Friend friend={friend} />;
 };
 
 export default FriendPage;
