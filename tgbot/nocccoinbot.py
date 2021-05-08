88b8bb5bf44da853c9f493e409a97905b27ca347
Zwenkka <henry.pietila@aalto.fi>
2021-05-08 13:13:21 +0300

    Added mining to tg-bot

--- tgbot/nocccoinbot.py
+++ tgbot/nocccoinbot.py
@@ -1,4 +1,6 @@
 import telebot
+import requests
+import base64
 
 API_TOKEN = "1807161420:AAHOFjG9SS5hwb62DKhRSFmP_fmA7nsLLa8"
 bot = telebot.TeleBot(API_TOKEN)
@@ -15,16 +17,28 @@ def help_message(message):
 def send_nocccoin(message):
     bot.send_photo(message.chat.id, photo=open('test/nocccoin.jpg', 'rb'))
 
+
 @bot.message_handler(content_types=['photo'])
-def photo(message):
-    print('message.photo =', message.photo)
-    fileID = message.photo[-1].file_id
-    print('fileID =', fileID)
-    file_info = bot.get_file(fileID)
-    print('file.file_path =', file_info.file_path)
-    downloaded_file = bot.download_file(file_info.file_path)
-    with open("image.jpg", 'wb') as new_file:
-        new_file.write(downloaded_file)
+def mine(message):
+    try:
+        print('message.photo =', message.photo)
+        fileID = message.photo[-1].file_id
+        print('fileID =', fileID)
+        file_info = bot.get_file(fileID)
+        print('file.file_path =', file_info.file_path)
+        downloaded_file = bot.download_file(file_info.file_path)
+
+        with open("image.jpg", 'wb') as new_file:
+            new_file.write(downloaded_file)
+
+        url = 'http://localhost:8000/nocccoins/mine'
+        data = {'user_id': '6095b6e5aeb80e67b4e4006c', 'image': downloaded_file}
+        headers = {'content-type:': 'application/json'}
+        response = requests.post(url,headers = headers, data = data)
+        print(response)
+    except:
+        print("vituixmän")
+
 
 
 
