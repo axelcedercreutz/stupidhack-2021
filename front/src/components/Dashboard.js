8a7d5f098def227c61f4bd7795eacfba63cb5f31
Axel Cedercreutz <axelcedercreutz@users.noreply.github.com>
2021-05-08 17:21:33 +0300

    Update Dashboard.js

--- front/src/components/Dashboard.js
+++ front/src/components/Dashboard.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import {
-  Table,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-} from '@material-ui/core';
 import useStore from '../store';
 
 const Dashboard = () => {
   const { userInfo } = useStore(state => state);
-  console.log(userInfo);
   return Array.isArray(userInfo.flavours) ? (
     <div>
       <h3>Your Noccoflavours</h3>
